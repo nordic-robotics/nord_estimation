e2eaf9023412f5942446b1fb4917e701650d781f
Lucas Åström <lucasas@kth.se>
2015-10-26 14:10:43 +0100

    map drawing

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -35,6 +35,35 @@ map read_map(std::string filename)
     return map(walls, min_x, min_y, max_x, max_y);
 }
 
+visualization_msgs::Marker create_map_message(const map& maze)
+{
+    visualization_msgs::Marker line_list;
+    line_list.id = 2;
+    line_list.type = visualization_msgs::Marker::LINE_LIST;
+    line_list.color.a = line_list.color.r = line_list.color.g = line_list.color.b = 1.0;
+    line_list.header.frame_id = "/map";
+    line_list.header.stamp = ros::Time::now();
+    line_list.ns = "particle_filter";
+    line_list.action = visualization_msgs::Marker::ADD;
+    line_list.pose.orientation.w = 1.0;
+    line_list.lifetime = ros::Duration();
+    line_list.scale.x = 0.01;
+
+    for (auto& wall : maze.get_walls())
+    {
+        geometry_msgs::Point p0, p1;
+        p0.x = wall.start.x();
+        p0.y = wall.start.y();
+        p1.x = wall.end.x();
+        p1.y = wall.end.y();
+        p0.z = p1.z = 0;
+        line_list.points.push_back(p0);
+        line_list.points.push_back(p1);
+    }
+
+    return line_list;
+}
+
 int main(int argc, char** argv)
 {
     using geometry_msgs::Pose2D;
@@ -52,6 +81,12 @@ int main(int argc, char** argv)
                                     0.049675f, 0.2015f));
 
     ros::Publisher guess_pub = n.advertise<Pose2D>("/nord/estimation/largest_weight", 10);
+
+    auto map_msg = create_map_message(maze);
+    auto map_pub = n.advertise<visualization_msgs::Marker>("/nord/map", 1);
+    auto map_timer = n.createTimer(ros::Duration(1), [&](const ros::TimerEvent& e) {
+        map_pub.publish(map_msg);
+    });
 
     pose odometry_prev;
     ros::Rate r(10);
